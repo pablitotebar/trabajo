16f1b3ae1bcf30728be8a47d3cefc6a3b24f97bd
AzureAD\PabloMuñozTébar <2021353@iesluisbraille.es>
2022-01-27 09:25:01 +0100

    version 1 suma

---
+++ version1 suma.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 09:19:48 2022
+
+@author: PabloMuñozTébar
+"""
+print("\t1.- Suma de dos números            --")
+opc = int(input("Introduzca una opción: "))
+if(opc==1):
+    x=int(input("Introduzca el primer número: "))
+    y=int(input("Introduzca el segundo número: "))
+    print(f"{x}+{y}={x+y}")
