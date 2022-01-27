2b99711c671d58dcaf6003aefec83c912581ab9b
AzureAD\PabloMuñozTébar <2021353@iesluisbraille.es>
2022-01-27 09:48:15 +0100

    v2

--- trabajo git.py
+++ trabajo git.py
@@ -5,8 +5,14 @@ Created on Thu Jan 27 09:19:48 2022
 @author: PabloMuñozTébar
 """
 print("\t1.- Suma de dos números            --")
+print("\t2.- Resta de dos números           --")
 opc = int(input("Introduzca una opción: "))
 if(opc==1):
     x=int(input("Introduzca el primer número: "))
     y=int(input("Introduzca el segundo número: "))
     print(f"{x}+{y}={x+y}")
+    
+elif(opc==2):
+    x=int(input("Introduzca el primer número: "))
+    y=int(input("Introduzca el segundo número: "))
+    print(f"{x}-{y}={x-y}")
